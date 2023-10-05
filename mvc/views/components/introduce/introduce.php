8f788f8a2e83c8e14b338fad4be52f09e7ea8a0e
aoisensei <yatogamitohka85@gmail.com>
2023-10-05 17:06:29 +0700

    add introduce

---
+++ mvc/views/components/introduce/introduce.php
@@ -0,0 +1,43 @@
+<style>
+    <?php 
+        include "./mvc/views/components/introduce/introduce.css";
+    ?>
+</style>
+
+<div class="home__introduce">
+    <div  class="home__introduce__content">
+        <h1 class="home__introduce__content__title">Về chúng tôi</h1>
+        <div class="home__introduce__content__description">
+            <span>Midori Boutique International Hotel Hanoi nằm ngay tại trung tâm Phố Cổ Hà Nội
+                cung cấp nhiều loại phòng nghỉ với đầy đủ tiện nghi với Wi-Fi miễn phí và phòng 
+                tắm  riêng trong mỗi phòng khách sạn đều được trang bị bồn tắm đứng và bồn tắm 
+                nằm. Khách sạn Midori Boutique nằm trong bán kính chỉ 900 m từ Hồ Hoàn Kiếm. Khách 
+                sạn có các tiện nghi như phòng tập Gym, quầy Bar, nhà hàng, sảnh chờ, massage 
+                service, Travel deck, Car Rental.
+            </span>
+            <span>Các phòng nghỉ tại Midori Boutique Hanoi Hotel có máy điều hòa hai chiều, nội thất 
+                sang trọng, không gian rộng rãi và cách âm tiêu chuẩn Châu Âu. Các phòng sáng sủa, 
+                thoáng mát trong phòng được trang bị tủ lạnh mini, bàn làm việc và bồn tắm trong 
+                phòng tắm. Máy pha trà cà phê, TV truyền hình vệ tinh và két an toàn cũng nằm trong 
+                số các tiện nghi của phòng nghỉ. Một số phòng nhìn ra quang cảnh Khu Phố Pháp, Hồ 
+                Gươm đặc biệt không gian yên ả là con phố Pháp cổ Triệu Việt Vương. Du khách có 
+                thể rèn luyện sức khỏe tại phòng tập thể dục hoặc đi bộ quanh khu vực phố cổ Hà nội 
+                hay Hồ Gươm để khám phá thành phố.
+            </span>
+            <span>Nhà hàng của Midori Boutique Hotel phục vụ bữa sáng cũng như tuyển chọn các món ăn 
+                Việt Nam và phương Tây theo yêu cầu của quý khách Nhà Hàng chúng tôi phục vụ các buổi 
+                sáng và chiều giờ bắt đầu phục vụ buffet ăn sáng từ 6h00 đến 10 h 00 buổi trưa từ 11h 
+                đến 13h30 buổi chiều từ 18h đến 21h30.
+            </span>
+            <span>Midori Boutique Hotel cách Hồ Hoàn Kiếm cũng như trung tâm mua sắm Vincom 5 phút 
+                đi bộ. Từ khách sạn du khách đi bộ 20 phút là đến Chợ Đồng Xuân cũng như Văn Miếu-Quốc 
+                Tử Giám. Ga Hà Nội nằm trong bán kính 0,8 km từ khách sạn tới Lăng Chủ tịch Hồ Chí Minh 
+                và Chùa Một Cột đều cách đó 2,5 km. Từ khách sạn Du khách chỉ mất  25 phút là đến sân 
+                bay quốc tế Nội Bài.
+            </span>
+        </div>
+    </div>
+    <div class="home__introduce__image">
+        <img src="https://monozy.net/wp-content/uploads/2019/04/khach-san-chup-anh-dep-o-ha-noi-4.jpg" alt="">
+    </div>
+</div>
